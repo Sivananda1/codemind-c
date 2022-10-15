9271ad64aaf1903daf88c9bb1ba754bb28bd5163
Sivananda1 <110597219+Sivananda1@users.noreply.github.com>
2022-10-15 22:14:23 +0530

    #include<stdio.h> int main() {     int a,b,c;     float si;     scanf("%d",&a);     scanf("%d",&b);     scanf("%d",&c);     si=(a*b*c)/100;     printf("%.0f",si); }

---
+++ SIMPLE_INTEREST.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c;
+    float si;
+    scanf("%d",&a);
+    scanf("%d",&b);             
+    scanf("%d",&c);
+    si=(a*b*c)/100;
+    printf("%.0f",si);
+}
