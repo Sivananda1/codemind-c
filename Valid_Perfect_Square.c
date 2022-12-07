63cb6f279f6ce94734f7853ea12d5ce0aee08980
Sivananda1 <110597219+Sivananda1@users.noreply.github.com>
2022-12-07 18:16:02 +0530

    #include<bits/stdc++.h> using namespace std; int main(){     int t;     cin>>t;     while (t){         t--;         int n;         cin>>n;         if ((int)sqrt(n)==sqrt(n)) cout<<"True"<<endl;         else cout<<"False"<<endl;     } }

---
+++ Valid_Perfect_Square.c
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while (t){
+        t--;
+        int n;
+        cin>>n;
+        if ((int)sqrt(n)==sqrt(n)) cout<<"True"<<endl;
+        else cout<<"False"<<endl;
+    }
+}
